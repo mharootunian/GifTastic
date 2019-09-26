aad70a4d7626c493a068d7f2efded5790bde9572
Meero Harootunian <meero.harootunian@gmail.com>
2019-09-25 23:42:52 -0700

    various additions and changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,57 +1,85 @@
-let apiURL = "https://api.giphy.com/v1/gifs/search?"
-let apiKey = "vO3MPzjFmhBfP3kRaLwBK51EIbKfXExm"
+$(document).ready(function () {
 
-topics = ["rofl", "roflcopter", "lmao", "lol", "gtfo"]
+	let apiURL = "https://api.giphy.com/v1/gifs/search?"
+	let apiKey = "vO3MPzjFmhBfP3kRaLwBK51EIbKfXExm"
 
-function buildQuery(query, limit, rating) {
-	let queryURL = apiURL;
-	//add query to GET request
-	queryURL += `q=${query}&`
+	topics = ["rofl", "roflcopter", "lmao", "lol", "gtfo"]
 
-	//add limit
-	queryURL += `limit=${limit}&`
-
-	//set language to english
-	queryURL += "lang=en&"
-
-	//add rating
-	queryURL += `rating=${rating}&`
-
-	//add apiKey
-	queryURL += `api_key=${apiKey}`
-
-	return queryURL
-}
-
-function buildHTML(response) {
-	let bodyDiv = $("<div>");
-
-	for (let i = 0; i < response.length; i++) {
-		let img = $("<img>")
-		img.attr("src", response[i].imgUrl)
-		bodyDiv.append(img)
+	function buildButtons(wordArray) {
+		wordArray.forEach(word => {
+			let btn = $("<button>")
+			btn.text(word)
+			btn.attr("class", "queryButton")
+			$("#buttons-container").append(btn)
+		});
 	}
 
-	$("#images-container").append(bodyDiv)
-}
+	function buildQuery(query, limit, rating) {
+		let queryURL = apiURL;
+		//add query to GET request
+		queryURL += `q=${query}&`
 
-function search(query, limit, rating) {
-	$.ajax({
-		url: buildQuery(query, limit, rating),
-		method: "GET"
-	}).then(function(response) {
-		buildHTML(response)
-	}, function(errResponse) {
+		//add limit
+		queryURL += `limit=${limit}&`
 
-	});
-}
+		//set language to english
+		queryURL += "lang=en&"
 
-$(function() {
-	let keyword = $("#keyword-input").val().trim()
+		//add rating
+		queryURL += `rating=${rating}&`
+
+		//add apiKey
+		queryURL += `api_key=${apiKey}`
+
+		return queryURL	
+	}
+
+	function buildHTML(data) {
+		let bodyDiv = $("<div>");
+		for (let i = 0; i < data.length; i++) {
+			let img = $("<img>")
+			img.attr("src", data[i].images.fixed_width_still.url)
+			console.log("building html...")
+			console.log(data[i].images.fixed_width_still.url)
+			bodyDiv.append(img)
+		}
+
+		$("#images-container").prepend(bodyDiv)
+	}
+
+	function search(query, limit, rating) {
+		$.ajax({
+			url: buildQuery(query, limit, rating),
+			method: "GET"
+		}).then(function (response) {
+			console.log(response.data[1])
+			buildHTML(response.data)
+		}, function (errResponse) {
+
+		});
+	}
+
+	buildButtons(topics)
 
 
-	$("#search-submit").click(function() {
-		
-
+	$("#search-submit").click(function () {
+		console.log("search submitted")
+		console.log($("#search-query").val())
+		search($("#search-query").val(), 10, "G")
 	})
+
+
+	$("#search-add").on("click", function () {
+		topics.push( $("#search-query").val() )
+		$("#buttons-container").empty()
+		buildButtons(topics)
+	})
+
+	$(document).on("click", ".queryButton", function () {
+		console.log("query button clicked")
+		search($(this).text(), 10, "g")
+	})
+
+
+
 })
